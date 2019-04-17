54190a88b9166bb347d2afe07ccccd51b2c3145e
Styort <styort02@gmail.com>
2019-04-17 19:16:03 +0300

    версия 1.2.4.0

--- src/MVVM/Views/MainWindow.xaml.cs
+++ src/MVVM/Views/MainWindow.xaml.cs
@@ -57,6 +57,7 @@ namespace LogViewer.MVVM.Views
                 string[] activationData = AppDomain.CurrentDomain.SetupInformation.ActivationArguments.ActivationData;
                 foreach (var arg in activationData.Where(x => x.EndsWith(".txt") || x.EndsWith(".log")))
                 {
+                    ((LogViewModel)DataContext).IsManualStartup = true;
                     ((LogViewModel)DataContext).ImportLogs(arg);
                 }
                 return;
@@ -69,6 +70,7 @@ namespace LogViewer.MVVM.Views
                 {
                     if (arg.EndsWith(".txt") || arg.EndsWith(".log"))
                     {
+                        ((LogViewModel)DataContext).IsManualStartup = true;
                         ((LogViewModel)DataContext).ImportLogs(arg);
                     }
                 }
